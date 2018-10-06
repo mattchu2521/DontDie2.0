90a62795c11f071786afaff84e01e400c2738849
Matt <matthew.chu@otterbein.edu>
2018-10-05 21:55:16 -0400

    Change MC sprite, change some file and class names and add mor sprites

--- desktop/src/com/fuck/game/desktop/DesktopLauncher.java
+++ desktop/src/com/fuck/game/desktop/DesktopLauncher.java
@@ -2,14 +2,14 @@ package com.fuck.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.fuck.game.Fuck;
+import com.dontdie.game.DontDie;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = Fuck.WIDTH;
-		config.height = Fuck.HEIGHT;
-		config.title = Fuck.TITLE;
-		new LwjglApplication(new Fuck(), config);
+		config.width = DontDie.WIDTH;
+		config.height = DontDie.HEIGHT;
+		config.title = DontDie.TITLE;
+		new LwjglApplication(new DontDie(), config);
 	}
 }
